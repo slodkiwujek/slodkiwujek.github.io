18305963703acec3695b9c6d261a5487e0c72878
slodkiwujek <121790364+slodkiwujek@users.noreply.github.com>
2023-01-02 09:04:48 +0100

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,10 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+// add a piece of text at position (120, 80)
+add([
+    text("hello"),
+    pos(120, 80),
+]);
